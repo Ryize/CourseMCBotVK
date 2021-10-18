59bc33db88197532ce12a47d8672b8f3183f05f8
CMatvey <chekashovmatvey@gmail.com>
2021-10-18 17:41:55 +0300

    Fix bug in /note and add new command /msg

--- vk_learn/__init__.py
+++ vk_learn/__init__.py
@@ -1,2 +1,3 @@
 __version__ = '1.0.0'
 __version__ = '1.1.1' #Add command /helpop, fix bug in param in command
+__version__ = '1.2.0' #Add command /msg, fix bug in command /note (If write group is not found)
