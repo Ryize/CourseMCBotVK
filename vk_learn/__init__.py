8b5a8f39f122d2ff057b28d20f9dcf65e18c75d3
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2021-10-31 12:07:57 +0300

    Update __init__.py
    Add new version 1.2.8

--- vk_learn/__init__.py
+++ vk_learn/__init__.py
@@ -8,3 +8,4 @@ __version__ = '1.2.4' #Add func get_command_text, fix bug in command with args
 __version__ = '1.2.5' #Fix bug in command /msg
 __version__ = '1.2.6' #Fix bug in command /msg (Bug in user_id)
 __version__ = '1.2.7' #Fix bug in /schedule
+__version__ = '1.2.8' #Fix bug in /anote
